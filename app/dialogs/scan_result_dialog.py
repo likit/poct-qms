e5bfba1f1e08be64a9cefbae6386329aef21c389
Likit Preeyanon <likit.pre@mahidol.edu>
2021-09-12 22:30:12 +0700

    expand the size of the dialog
    added error cause combobox to allow user to modify the cause of error

--- app/dialogs/scan_result_dialog.py
+++ app/dialogs/scan_result_dialog.py
@@ -1,31 +1,38 @@
 import wx
-from app.models.values import TestStatus
+from app.models.values import TestStatus, ErrorCause
 from ObjectListView import ObjectListView, ColumnDefn, Filter
 
 
 class ScanResultDialog(wx.Dialog):
     def __init__(self, parent, records, columns, session, title='', **kwargs):
-        super(ScanResultDialog, self).__init__(parent, title=title, size=(640, 480), **kwargs)
+        super(ScanResultDialog, self).__init__(parent, title=title, size=(1200, 800), **kwargs)
         self.columns = columns
         self.records = records
         vsizer = wx.BoxSizer(wx.VERTICAL)
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.choices = [''] + [item.status for item in session.query(TestStatus).all()]
+        self.status_choices = [''] + [item.status for item in session.query(TestStatus).all()]
+        self.error_choices = [''] + [item.cause for item in session.query(ErrorCause).all()]
         button_sizer = wx.StaticBoxSizer(wx.VERTICAL, self, label='Edit')
-        comment_label = wx.StaticText(self, label='Payment Status')
+        status_label = wx.StaticText(self, label='Payment Status')
         self.status_entry = wx.ComboBox(self, value='',
-                                        choices=self.choices,
+                                        choices=self.status_choices,
+                                        style=wx.CB_SORT | wx.CB_READONLY)
+        error_label = wx.StaticText(self, label='Error')
+        self.error_entry = wx.ComboBox(self, value='',
+                                        choices=self.error_choices,
                                         style=wx.CB_SORT | wx.CB_READONLY)
         save_button = wx.Button(self, label='Save')
         save_button.Bind(wx.EVT_BUTTON, self.onSaveButtonClicked)
-        button_sizer.Add(comment_label)
-        button_sizer.Add(self.status_entry)
+        button_sizer.Add(status_label)
+        button_sizer.Add(self.status_entry, 1, wx.EXPAND)
+        button_sizer.Add(error_label)
+        button_sizer.Add(self.error_entry, 1, wx.EXPAND)
         button_sizer.Add(save_button, 0, wx.TOP | wx.EXPAND, 20)
         buttons = self.CreateButtonSizer(wx.CANCEL | wx.OK)
         self.dataOlv = ObjectListView(self, style=wx.LC_REPORT | wx.SUNKEN_BORDER)
         self.dataOlv.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onItemSelected)
         hsizer.Add(self.dataOlv, 1, wx.EXPAND | wx.LEFT | wx.TOP, 10)
-        hsizer.Add(button_sizer, 0, wx.EXPAND | wx.RIGHT, 10)
+        hsizer.Add(button_sizer, 0, wx.RIGHT, 10)
         vsizer.Add(hsizer, 1, wx.EXPAND)
         vsizer.Add(buttons)
         self.SetSizer(vsizer)
@@ -46,11 +53,13 @@ class ScanResultDialog(wx.Dialog):
 
     def onItemSelected(self, event):
         selected_row = self.dataOlv.GetSelectedObject()
-        choice_idx = self.status_entry.FindString(selected_row.payment_status)
-        self.status_entry.SetSelection(choice_idx)
+        status_idx = self.status_entry.FindString(selected_row.payment_status)
+        error_idx = self.error_entry.FindString(selected_row.error_cause)
+        self.status_entry.SetSelection(status_idx)
+        self.error_entry.SetSelection(error_idx)
 
     def onSaveButtonClicked(self, event):
         selected_row = self.dataOlv.GetSelectedObject()
-        print(self.status_entry.GetStringSelection())
         selected_row.payment_status = self.status_entry.GetStringSelection()
+        selected_row.error_cause = self.error_entry.GetStringSelection()
         self.dataOlv.RefreshObject(selected_row)
