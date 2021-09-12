0b600086438c845f3713e88b92143c9813e36d00
Likit Preeyanon <likit.pre@mahidol.edu>
2021-09-12 23:53:10 +0700

    added engine and base for patient dw

--- app/extension.py
+++ app/extension.py
@@ -4,6 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///qms.db')
 Base = declarative_base()
 
+patient_engine = create_engine('sqlite:///patientdw.db')
+PatientBase = declarative_base()
+
 
 class GenericDBClass():
     pass
